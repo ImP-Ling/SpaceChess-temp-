4999e14862f07c5fab9e5d90d95522e606865757
ImaginePilot <ImaginePilot@DESKTOP-0A6R2SH>
2021-07-26 20:17:38 +0800

    added ship class

---
+++ Ships.py
@@ -0,0 +1,50 @@
+
+
+'''
+a series of ships classes
+'''
+import pygame
+
+class Ships:
+    def __init__ (self,surface,x,y,player):
+        self.surface=surface
+        self.player=player
+        self.x=x
+        self.y=y
+        self.player=player
+        self.cost=None
+        self.weapon_cost=None
+        self.move_cost=None
+        self.image=None
+        self.damage=None
+        self.health=None
+        self.weapon_type=0
+        self.armour_type=0
+        self.image.blit(self.surface,(16*x,16*y))
+
+    def move(self,x,y):
+        cost=self.move_cost*(abs(self.x-x)+abs(self.y-y))
+        rect=pygame.draw.rect(self.surface,(255,255,255),self.image.get_rect())
+        rect.blit(self.surface,(16*self.x,16*self.y))
+        self.x=x
+        self.y=y
+        self.image.blit(self.surface,(16*self.x,16*self.y))
+        return cost
+
+    def attack(self,target):
+        target.damage(self.damage,self.weapon_type)
+        clock=pygame.time.Clock()
+        if self.weapon_type == 1:
+            i=0
+            while(i<60):
+                clock.tick(60)
+                pygame.draw.line(self.surface,(255,0,0),(16*self.x,16*self.y),(16*target.x,16*target.y),int(i/6))
+                i=i+1
+        # unfinished railgun animation
+        return self.weapon_cost
+
+    def damage(self,damage,weapon_type):
+        if(abs(weapon_type-self.armour_type)==0):
+            self.health=self.health-int(damage/2)
+        else:
+            self.health=self.health-damage
